c16cd0353e40b8d2dc432c9fee69dda832207a61
SweetieMetalhead <paul.lv.ohl@gmail.com>
2020-05-24 09:45:52 +0200

    fixed ft_read and ft_write by saving the registers

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -5,16 +5,24 @@
 		section	.text
 
 _ft_read:
+		push	rdi
+		push	rsi
+		push	rdx
 		mov		rax, 0x02000003	; system call for read
 		syscall					; invoke operating system to do the read
-		cmp		rdx, 0
-		jne		error
+		jc		error
+		pop		rdi
+		pop		rsi
+		pop		rdx
 		ret
 
 error:
 		mov		r9, rax
 		call	___error
-		mov		byte [rax], r9b
+		mov		dword [rax], r9d
 		sub		rax, rax
 		dec		rax				; set rax to -1
+		pop		rdi
+		pop		rsi
+		pop		rdx
 		ret
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,24 +1,28 @@
 ; inputs : rdi = fd ; rsi = buf ; rdx = nbytes
-; Problem: how to detect wrong fd
 
 		global	_ft_write
 		extern ___error
 		section	.text
 
 _ft_write:
+		push	rdi
+		push	rsi
 		push	rdx
 		mov		rax, 0x02000004	; system call for write
 		syscall					; invoke operating system to do the write
-		cmp		rdx, 0
-		jne		error
-		pop		rax
+		jc		error
+		pop		rdi
+		pop		rsi
+		pop		rdx
 		ret
 
 error:
 		mov		r9, rax
 		call	___error
-		mov		byte [rax], r9b
-		pop		rax
+		mov		dword [rax], r9d
 		sub		rax, rax
 		dec		rax				; set rax to -1
+		pop		rdi
+		pop		rsi
+		pop		rdx
 		ret
